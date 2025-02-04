3aa1af0aef2b2f32f1c080d9ce71f1417d59f9b9
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-02-04 21:04:40 +0530

    added association for user and role

---
+++ src/migrations/20250204144444-create-role.js
@@ -0,0 +1,32 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.ENUM,
+        values:['customer','flight_company','admin'],
+        defaultValue:'customer',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Roles');
+  }
+};
+
+//I HAVE SEVERE DOUBT ON TABLE ASSOCIATION NOT UNDERSTOOD ANYTHING>>> IN ASSOCIATION>>
