b119e31f4801170197abb66d4557b4dfec1afd09
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-02-04 16:18:37 +0530

    added rate limiter

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
-const {Logger}=require('./config')
+const {Logger}=require('./config');
+const rateLimit=require('express-rate-limit');
 const app = express();
+
+const limiter=rateLimit({
+    windowMs:2*60*1000, //2 minutes
+    max:50,//limit each ip to 50 request per window
+    message: {
+        status: 429,
+        error: "Too many request",
+        message: "You have exceeded the request limit. Please try again later."
+    },
+});
+
+app.use(limiter);
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
